b374e8e49f62fa48281dba58e56e221625d20d30
manuela <manuela.stolz@gmail.com>
2021-12-03 14:08:47 +0100

    solve puzzle 2

--- 2021.03/src/mama/aoc/Diagnosis.java
+++ 2021.03/src/mama/aoc/Diagnosis.java
@@ -13,43 +13,69 @@ public class Diagnosis {
     public int diagnosis(String pathToDiagnosis) {
         List<String> fullDiagnosis = FileReader.readFile(pathToDiagnosis);
 
-        //initialize arrays to count with 0
-        int lengthOfDiagnosis = fullDiagnosis.stream().findAny().get().length();
-        for (int i = 0; i < lengthOfDiagnosis; i++) {
-            numberOfZeroes.add(0);
-            numberOfOnes.add(0);
-        }
+        initializeArrays(fullDiagnosis);
 
+        //count 0 and 1 at each position
         for (String diagnosis : fullDiagnosis) {
 //            System.out.println("Binary diagnosis: " + diagnosis);
-            countZeroesAndOnes(diagnosis);
+            for (int i = 0; i < diagnosis.length(); i++) {
+                countZeroesAndOnes(diagnosis, i);
+            }
         }
 
-        calculateGammaAndEpsilon(lengthOfDiagnosis);
+        calculateGammaAndEpsilon(numberOfOnes.size());
         int gammaDec = Integer.parseInt(gamma.toString(), 2);
         int epsilonDec = Integer.parseInt(epsilon.toString(), 2);
 
         return gammaDec * epsilonDec;
     }
 
-    private void countZeroesAndOnes(String diagnosis) {
-        for (int i = 0; i < diagnosis.length(); i++) {
-            switch (diagnosis.charAt(i)) {
-                case '0':
-                    int count = numberOfZeroes.get(i);
-                    numberOfZeroes.set(i, ++count);
-                    break;
-                case '1':
-                    Integer integer = numberOfOnes.get(i);
-                    integer++;
-                    numberOfOnes.set(i, integer);
-                    break;
-                default:
-                    System.out.println(diagnosis.charAt(i) + " results in default case");
-            }
+    public int lifeSupportRating(String pathToDiagnosis) {
+        List<String> fullDiagnosis = FileReader.readFile(pathToDiagnosis);
+        initializeArrays(fullDiagnosis);
+
+        //Oxygen rating
+        String finalRating = oxygenRating(fullDiagnosis, 0);
+        System.out.println("Oxygen: Binaer: " + finalRating);
+        int oxygen = Integer.parseInt(finalRating, 2);
+        System.out.println("Oxygen Dezimal: " + oxygen);
+        System.out.println("");
+
+        initializeArrays(fullDiagnosis);
+        String finalCo2Rating = co2Rating(fullDiagnosis, 0);
+        System.out.println("Co2: Binaer: " + finalCo2Rating);
+        int co2 = Integer.parseInt(finalCo2Rating, 2);
+        System.out.println("Co2 Dezimal: " + co2);
+
+        System.out.println("\nLife Support Rating: " + oxygen * co2);
+        return oxygen * co2;
+    }
+
+    private void initializeArrays(List<String> fullDiagnosis) {
+        //initialize arrays to count with 0
+        int lengthOfDiagnosis = fullDiagnosis.stream().findAny().get().length();
+        numberOfZeroes = new ArrayList<>(lengthOfDiagnosis);
+        numberOfOnes = new ArrayList<>(lengthOfDiagnosis);
+        for (int i = 0; i < lengthOfDiagnosis; i++) {
+            numberOfZeroes.add(0);
+            numberOfOnes.add(0);
         }
-//        System.out.println("Zeroes: " + numberOfZeroes);
-//        System.out.println("Ones: " + numberOfOnes);
+    }
+
+    private void countZeroesAndOnes(String diagnosis, int position) {
+        int count;
+        switch (diagnosis.charAt(position)) {
+            case '0':
+                count = numberOfZeroes.get(position);
+                numberOfZeroes.set(position, ++count);
+                break;
+            case '1':
+                count = numberOfOnes.get(position);
+                numberOfOnes.set(position, ++count);
+                break;
+            default:
+                System.out.println(diagnosis.charAt(position) + " results in default case");
+        }
     }
 
     private int calculateGammaAndEpsilon(int lengthOfInput) {
@@ -61,9 +87,64 @@ public class Diagnosis {
                 this.gamma.append(1);
                 this.epsilon.append(0);
             }
-//            System.out.println("Gamma: " + gamma.toString());
-//            System.out.println("Epsilon: "+ epsilon.toString());
         }
         return 0;
     }
+
+    private String oxygenRating(List<String> fullDiagnosis, int position) {
+        for (String diagnosis :
+                fullDiagnosis) {
+            countZeroesAndOnes(diagnosis, position);
+        }
+//        System.out.println("0: " + numberOfZeroes);
+//        System.out.println("1: " + numberOfOnes);
+
+        List<String> reducedList;
+        if (numberOfZeroes.get(position) > numberOfOnes.get(position)) { // there are more zeroes, keep zeroes
+            reducedList = keepNumbersWithBitAtPosition(fullDiagnosis, position, '0');
+        } else {
+            reducedList = keepNumbersWithBitAtPosition(fullDiagnosis, position, '1');
+        }
+
+        if (reducedList.size() > 1) {
+            return oxygenRating(reducedList, ++position);
+        } else {
+            return reducedList.get(0);
+        }
+    }
+
+    private String co2Rating(List<String> fullDiagnosis, int position) {
+        for (String diagnosis :
+                fullDiagnosis) {
+            countZeroesAndOnes(diagnosis, position);
+        }
+//        System.out.println("0: " + numberOfZeroes);
+//        System.out.println("1: " + numberOfOnes);
+
+        List<String> reducedList;
+        if (numberOfZeroes.get(position) > numberOfOnes.get(position)) { // there are less ones, keep ones
+            reducedList = keepNumbersWithBitAtPosition(fullDiagnosis, position, '1');
+        } else {
+            reducedList = keepNumbersWithBitAtPosition(fullDiagnosis, position, '0');
+        }
+
+        if (reducedList.size() > 1) {
+            return co2Rating(reducedList, ++position);
+        } else {
+            return reducedList.get(0);
+        }
+    }
+
+    private List<String> keepNumbersWithBitAtPosition(List<String> fullDiagnosis, int position, char keepBit) {
+        List<String> reducedDiagnosis = new ArrayList<>();
+        for (String diagnosis : fullDiagnosis) {
+            if (diagnosis.charAt(position) == keepBit) {
+                reducedDiagnosis.add(diagnosis);
+            }
+        }
+//        System.out.println("Liste für Position " + position + " hat " + reducedDiagnosis.size() + " Elemente. Behalte: " + keepBit);
+        return reducedDiagnosis;
+    }
+
+
 }
--- 2021.03/test/mama/aoc/DiagnosisTest.java
+++ 2021.03/test/mama/aoc/DiagnosisTest.java
@@ -7,19 +7,37 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DiagnosisTest {
 
+    private String smallDiagnosisPath = "/home/manuela/projects/AdventOfCode/2021.03/test/mama/aoc/DiagnosisSmall.txt";
+    private String bigDiagnosisPath = "/home/manuela/projects/AdventOfCode/2021.03/test/mama/aoc/DiagnosisBig.txt";
+
     @Test
     public void checkDiagnosisSmall() {
         Diagnosis submarine = new Diagnosis();
-        int i = submarine.diagnosis("/home/manuela/projects/AdventOfCode/2021.03/test/mama/aoc/DiagnosisSmall.txt");
+        int i = submarine.diagnosis(smallDiagnosisPath);
+        System.out.println("Small diagnosis: " + i);
         assertEquals(198, i, "Multiplication of gamma and epsilon diagnosis");
     }
 
     @Test
     public void checkDiagnosisBig() {
         Diagnosis submarine = new Diagnosis();
-        int i = submarine.diagnosis("/home/manuela/projects/AdventOfCode/2021.03/test/mama/aoc/DiagnosisBig.txt");
+        int i = submarine.diagnosis(bigDiagnosisPath);
         System.out.println("Big diagnosis: " + i);
         assertTrue(i > 0, "Multiplication of gamma and epsilon diagnosis for big diagnosis");
+    }
+
+    @Test
+    public void checkLifeSupportRatingSmall() {
+        Diagnosis submarine = new Diagnosis();
+        int i = submarine.lifeSupportRating(smallDiagnosisPath);
+        assertEquals(230, i);
+    }
+
+    @Test
+    public void checkLifeSupportRatingBig() {
+        Diagnosis submarine = new Diagnosis();
+        int i = submarine.lifeSupportRating(bigDiagnosisPath);
+        assertTrue(i > 0, "Multiplication of oxygen and co2 rating for big diagnosis");
     }
 
     @Test
