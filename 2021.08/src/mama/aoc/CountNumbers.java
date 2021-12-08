93750c26c3e7dcdd4d6e321fd08fb502059610d6
manuela <manuela.stolz@gmail.com>
2021-12-08 21:41:07 +0100

    first puzzle solved

--- 2021.08/src/mama/aoc/CountNumbers.java
+++ 2021.08/src/mama/aoc/CountNumbers.java
@@ -1,4 +1,33 @@
 package mama.aoc;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CountNumbers {
+
+    private List<Integer> uniqueAmountAsList = Arrays.asList(2, 3, 4, 7);
+    private int uniqueDigits = 0;
+
+    public int countUniqueDigitNumbers(String path) {
+        List<String> strings = FileReader.readFile(path);
+        for (String noteEntry : strings) {
+            String[] split = noteEntry.split(" \\| ");
+//            System.out.println("Digits displayed: " + split[1]);
+            String[] digits = split[1].split(" ");
+            countUniqueDigits(digits);
+//            System.out.println("");
+        }
+        System.out.println("Unique digits: " + uniqueDigits);
+        return uniqueDigits;
+    }
+
+    private void countUniqueDigits(String[] digits) {
+        for (String digit :
+                digits) {
+//            System.out.println("Digit " + digit + "\t has length " + digit.length());
+            if (uniqueAmountAsList.contains(digit.length())) {
+                uniqueDigits++;
+            }
+        }
+    }
 }
--- 2021.08/test/mama/aoc/CountNumbersTest.java
+++ 2021.08/test/mama/aoc/CountNumbersTest.java
@@ -1,7 +1,25 @@
 package mama.aoc;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountNumbersTest {
 
+    private String smallNumbersPath = "/home/manuela/projects/AdventOfCode/2021.08/test/mama/aoc/SmallNumbers.txt";
+    private String bigNumbersPath = "/home/manuela/projects/AdventOfCode/2021.08/test/mama/aoc/BigNumbers.txt";
+
+    @Test
+    public void countNumbersSmallTest() {
+        CountNumbers submarine = new CountNumbers();
+        int i = submarine.countUniqueDigitNumbers(smallNumbersPath);
+        assertEquals(26, i, "there are 26 unique digit numbers in the small input");
+    }
+
+    @Test
+    public void countNumbersBigTest() {
+        CountNumbers submarine = new CountNumbers();
+        int i = submarine.countUniqueDigitNumbers(bigNumbersPath);
+        assertTrue(i>0, "there are more than 0 unique digits in the big input");
+    }
 }
