4691c22f50d7a9460753e98810d5c4fa9dfb4935
manuela <manuela.stolz@gmail.com>
2021-12-08 23:24:18 +0100

    start of second puzzle

--- 2021.08/test/mama/aoc/CountNumbersTest.java
+++ 2021.08/test/mama/aoc/CountNumbersTest.java
@@ -10,16 +10,25 @@ class CountNumbersTest {
     private String bigNumbersPath = "/home/manuela/projects/AdventOfCode/2021.08/test/mama/aoc/BigNumbers.txt";
 
     @Test
-    public void countNumbersSmallTest() {
+    public void countUniqueNumbersSmallTest() {
         CountNumbers submarine = new CountNumbers();
         int i = submarine.countUniqueDigitNumbers(smallNumbersPath);
         assertEquals(26, i, "there are 26 unique digit numbers in the small input");
     }
 
     @Test
-    public void countNumbersBigTest() {
+    public void countUniqueNumbersBigTest() {
         CountNumbers submarine = new CountNumbers();
         int i = submarine.countUniqueDigitNumbers(bigNumbersPath);
         assertTrue(i>0, "there are more than 0 unique digits in the big input");
     }
+
+    @Test
+    public void countNumbersSmallTest() {
+        CountNumbers submarine = new CountNumbers();
+        int i = submarine.countNumbers(smallNumbersPath);
+        assertEquals(26, i, "there are 26 unique digit numbers in the small input");
+    }
+
+
 }
