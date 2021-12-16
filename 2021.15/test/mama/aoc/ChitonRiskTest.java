a9ceb1ce3024dbd23c4260786ab13e26880149e0
manuela <manuela.stolz@gmail.com>
2021-12-16 09:16:07 +0100

    Failed to solve even first puzzle of 2021-12-15

--- 2021.15/test/mama/aoc/ChitonRiskTest.java
+++ 2021.15/test/mama/aoc/ChitonRiskTest.java
@@ -1,8 +1,38 @@
 package mama.aoc;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChitonRiskTest {
+
+    String smallPath = "/home/manuela/projects/AdventOfCode/2021.15/test/mama/aoc/SmallChiton.txt";
+    String bigPath = "/home/manuela/projects/AdventOfCode/2021.15/test/mama/aoc/BigChiton.txt";
+    String verySmallPath = "/home/manuela/projects/AdventOfCode/2021.15/test/mama/aoc/VerySmallChiton.txt";
+
+    @Test
+    public void testCalculateLowestRiskVerySmall() {
+        ChitonRisk submarine = new ChitonRisk();
+        int i = submarine.calculateLowestRisk(verySmallPath);
+        System.out.println("minimum risk very small: " + i);
+        assertEquals(7, i);
+    }
+
+    @Test
+    public void testCalculateLowestRiskSmall() {
+        ChitonRisk submarine = new ChitonRisk();
+        int i = submarine.calculateLowestRisk(smallPath);
+        System.out.println("minimum risk small: " + i);
+        assertEquals(40, i);
+    }
+
+    @Test
+    public void testCalculateLowestRiskBig() {
+        ChitonRisk submarine = new ChitonRisk();
+        int i = submarine.calculateLowestRisk(bigPath);
+        System.out.println("minimum risk big: " + i);
+        assertEquals(40, i);
+    }
 
 
 }
--- commons/src/mama/aoc/StringHelper.java
+++ commons/src/mama/aoc/StringHelper.java
@@ -32,4 +32,22 @@ public class StringHelper {
         }
         return intList;
     }
+
+    /**
+     * Convert a list of Strings, which are basically numbers, to a double list (=list of list) of Integers.
+     *
+     * @param stringList a list of strings containing numbers
+     * @return a list of list of numbers (similar to Integer[][])
+     */
+    public static List<List<Integer>> convertToNumberDoubleList(List<String> stringList) {
+        List<List<Integer>> intMap = new ArrayList<>(stringList.size());
+        for (int i = 0; i < stringList.size(); i++) {
+            String line = stringList.get(i);
+            intMap.add(new ArrayList<>(line.length()));
+            for (Character character : line.toCharArray()) {
+                intMap.get(i).add(Integer.parseInt(character + ""));
+            }
+        }
+        return intMap;
+    }
 }
