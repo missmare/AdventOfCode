6c2c5c850f70adbbc059c0474bbcd4b53b4be1f4
manuela <manuela.stolz@gmail.com>
2022-02-09 09:51:53 +0100

    include visited concept into first version of algorithm

--- 2021.15/src/mama/aoc/algorithm/Chiton.java
+++ 2021.15/src/mama/aoc/algorithm/Chiton.java
@@ -18,6 +18,7 @@ public class Chiton {
 
         int step = 0;
 
+        boolean[][] visitedNodes = new boolean[maxX + 1][maxY + 1];
         List<RiskPath> possiblePaths = new ArrayList<>();
         RiskPath currentPath = new RiskPath();
         possiblePaths.add(currentPath);
@@ -31,26 +32,30 @@ public class Chiton {
             //check all positions
             int currentX = currentPath.getCurrentPosition().getFirst();
             int currentY = currentPath.getCurrentPosition().getSecond();
-/*            //up
-            if (currentY - 1 >= 0 && !currentPath.cameFrom(currentX, currentY - 1)) {
+
+            //set this position as visited
+            visitedNodes[currentX][currentY] = true;
+
+            //up
+            if (currentY - 1 >= 0 && !currentPath.cameFrom(currentX, currentY - 1) && !visitedNodes[currentX][currentY - 1]) {
                 RiskPath newRisk = currentPath.moveTo(currentX, currentY - 1, chitonList);
-                System.out.println("\tmove up to " + newRisk.getCurrentPosition() + " at risk " + newRisk.getTotalRisk());
+//                System.out.println("\tmove up to " + newRisk.getCurrentPosition() + " at risk " + newRisk.getTotalRisk());
                 possiblePaths.add(newRisk);
             }
             //left
-            if (currentX - 1 >= 0 && !currentPath.cameFrom(currentX - 1, currentY)) {
+            if (currentX - 1 >= 0 && !currentPath.cameFrom(currentX - 1, currentY) && !visitedNodes[currentX - 1][currentY]) {
                 RiskPath newRisk = currentPath.moveTo(currentX - 1, currentY, chitonList);
-                System.out.println("\tmove left to " + newRisk.getCurrentPosition() + " at risk " + newRisk.getTotalRisk());
+//                System.out.println("\tmove left to " + newRisk.getCurrentPosition() + " at risk " + newRisk.getTotalRisk());
                 possiblePaths.add(newRisk);
-            }*/
+            }
             //down
-            if (currentY + 1 <= maxY && !currentPath.cameFrom(currentX, currentY + 1)) {
+            if (currentY + 1 <= maxY && !currentPath.cameFrom(currentX, currentY + 1) && !visitedNodes[currentX][currentY + 1]) {
                 RiskPath newRisk = currentPath.moveTo(currentX, currentY + 1, chitonList);
 //                System.out.println("\tmove down to " + newRisk.getCurrentPosition() + " at risk " + newRisk.getTotalRisk());
                 possiblePaths.add(newRisk);
             }
             //right
-            if (currentX + 1 <= maxX && !currentPath.cameFrom(currentX + 1, currentY)) {
+            if (currentX + 1 <= maxX && !currentPath.cameFrom(currentX + 1, currentY) && !visitedNodes[currentX + 1][currentY]) {
                 RiskPath newRisk = currentPath.moveTo(currentX + 1, currentY, chitonList);
 //                System.out.println("\tmove right to " + newRisk.getCurrentPosition() + " at risk " + newRisk.getTotalRisk());
                 possiblePaths.add(newRisk);
