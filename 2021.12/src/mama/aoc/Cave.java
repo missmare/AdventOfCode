285aeb212648a6055864582791ed08a4313e964e
manuela <manuela.stolz@gmail.com>
2022-02-16 15:00:14 +0100

    solve fist puzzle of AoC 2021-12-12

--- 2021.12/src/mama/aoc/Cave.java
+++ 2021.12/src/mama/aoc/Cave.java
@@ -1,6 +1,70 @@
 package mama.aoc;
 
+import java.util.*;
+
 public class Cave {
+
+    private Map<String, List<String>> cave = new HashMap<>();
+    private List<List<String>> startedPaths = new ArrayList<>();
+    private List<List<String>> possiblePaths = new ArrayList<>();
+
+
+    public int countNumberOfPaths(String path) {
+        List<String> strings = FileReader.readFile(path);
+        for (String possiblePath : strings) {
+            String[] split = possiblePath.split("-");
+            if (!cave.containsKey(split[0])) {
+                cave.put(split[0], new ArrayList<>());
+            }
+            if (!cave.containsKey(split[1])) {
+                cave.put(split[1], new ArrayList<>());
+            }
+            cave.get(split[0]).add(split[1]);
+            cave.get(split[1]).add(split[0]);
+        }
+
+        startPossiblePaths();
+        return possiblePaths.size();
+    }
+
+    private void startPossiblePaths() {
+        List<String> adjacentCaves = cave.get("start");
+        for (String adjacentCave : adjacentCaves) {
+            List<String> possiblePath = new ArrayList<>();
+            possiblePath.add("start");
+            possiblePath.add(adjacentCave);
+            startedPaths.add(possiblePath);
+        }
+        calculatePaths();
+    }
+
+    private void calculatePaths() {
+        List<List<String>> currentPaths = new ArrayList<>();
+        for (List<String> path : startedPaths) {
+            String lastElement = path.get(path.size() - 1);
+            List<String> adjacentCaves = cave.get(lastElement);
+            for (String adjacentCave : adjacentCaves) {
+                //check rule
+                if (adjacentCave.equals("end")) {
+                    possiblePaths.add(path);
+                } else if (isAllowed(path, adjacentCave)) {
+                    List<String> newPath = new ArrayList<>(path);
+                    newPath.add(adjacentCave);
+                    currentPaths.add(newPath);
+                }
+            }
+        }
+        startedPaths = currentPaths;
+        if (!startedPaths.isEmpty()) {
+            calculatePaths();
+        }
+    }
+
+    private boolean isAllowed(List<String> path, String adjacentCave) {
+        return (adjacentCave.equals(adjacentCave.toLowerCase()) && !path.contains(adjacentCave))
+                || adjacentCave.equals(adjacentCave.toUpperCase());
+
+    }
 
 
 }
--- 2021.12/test/mama/aoc/CaveTest.java
+++ 2021.12/test/mama/aoc/CaveTest.java
@@ -6,9 +6,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CaveTest {
 
-    @Test
-    public void testSmallCave() {
+    String smallCave1 = "/home/manuela/projects/AdventOfCode/2021.12/test/mama/aoc/SmallCave1.txt";
+    String smallCave2 = "/home/manuela/projects/AdventOfCode/2021.12/test/mama/aoc/SmallCave2.txt";
+    String smallCave3 = "/home/manuela/projects/AdventOfCode/2021.12/test/mama/aoc/SmallCave3.txt";
+    String bigPath = "/home/manuela/projects/AdventOfCode/2021.12/test/mama/aoc/BigCave.txt";
 
+    @Test
+    public void testSmallCave1() {
+        Cave submarine = new Cave();
+        int i = submarine.countNumberOfPaths(smallCave1);
+        System.out.println("There are " + i + " paths through the cave");
+        assertEquals(10, i);
+    }
+
+    @Test
+    public void testSmallCave2() {
+        Cave submarine = new Cave();
+        int i = submarine.countNumberOfPaths(smallCave2);
+        System.out.println("There are " + i + " paths through the cave");
+        assertEquals(19, i);
+    }
+
+    @Test
+    public void testSmallCave3() {
+        Cave submarine = new Cave();
+        int i = submarine.countNumberOfPaths(smallCave3);
+        System.out.println("There are " + i + " paths through the cave");
+        assertEquals(226, i);
+    }
+
+
+    @Test
+    public void testBigCave() {
+        Cave submarine = new Cave();
+        int i = submarine.countNumberOfPaths(bigPath);
+        System.out.println("There are " + i + " paths through the cave");
+        assertEquals(3450, i);
     }
 
 }
